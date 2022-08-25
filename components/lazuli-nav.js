180753cac5714c05c2fe1f172ce2b6bf9eca27a4
lazuli0528 <106423208+lazuli0528@users.noreply.github.com>
2022-08-25 17:14:39 +0900

    Update lazuli-nav.js

--- components/lazuli-nav.js
+++ components/lazuli-nav.js
@@ -1,10 +1,14 @@
+import Link from 'next/link'
+
 export default function Nav(props) {
   return (
         <div className="bg-lazuli">
             <div className="container">
                 <nav className="navbar">
                     <div className="navbar-brand">
-                        <a href="/" className="btn">Lazuli</a>
+                        <Link href="/">
+                            <a className="btn">Lazuli</a>
+                        </Link>
                     </div>
                 </nav>
             </div>
